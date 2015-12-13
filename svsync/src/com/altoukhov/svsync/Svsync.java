e593520fdbc3dce07fb1e56577ce54a43c2a3b5a
Alex Altoukhov <sval00@gmail.com>
2015-12-13 15:56:37 -0800

    Updating license info

--- svsync/src/com/altoukhov/svsync/Svsync.java
+++ svsync/src/com/altoukhov/svsync/Svsync.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2015 Alex Altoukhov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.altoukhov.svsync;
 
 import java.util.HashMap;
@@ -6,11 +22,8 @@ import com.altoukhov.svsync.engines.Analyzer;
 import com.altoukhov.svsync.engines.Restorer;
 import com.altoukhov.svsync.engines.Syncer;
 
-//TODO: 1. do retries on read? read-write, not scan, maybe not required
-//TODO: 2. Lock specific profile, if it's running can't run more instances
-
 /**
- * @author Sval
+ * @author Alex Altoukhov
  */
 public class Svsync {
     
--- svsync/src/com/altoukhov/svsync/fileviews/IWriteableFileSpace.java
+++ svsync/src/com/altoukhov/svsync/fileviews/IWriteableFileSpace.java
@@ -1,10 +1,26 @@
+/*
+Copyright 2015 Alex Altoukhov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.altoukhov.svsync.fileviews;
 
 import java.io.InputStream;
 import com.altoukhov.svsync.FileSnapshot;
 
 /**
- * @author Sval
+ * @author Alex Altoukhov
  */
 public interface IWriteableFileSpace {
     boolean createDirectory(String path);
